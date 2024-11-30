7d17efeb82acf0bc50592f3c33a91549a82273fd
Ahmad Ali <xevenahmad@gmail.com>
2024-11-30 10:43:56 +0500

    explore react form builder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// src/App.jsx
-
+import 'react-form-builder2/dist/app.css';
 import FormBuilder from './components/FormBuilder';
 
 function App() {
   return (
     <div>
-      <h1>Form Builder</h1>
+      <h1>React Form Builder Example</h1>
       <FormBuilder />
     </div>
   );
